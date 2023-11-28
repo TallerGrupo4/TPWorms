3f8ab01b1fb268a3793180dbf8e5f19fa53de37e
LijsTh <lijsth@gmail.com>
2023-11-28 02:36:22 -0300

    Added explosion_callback.cpp

---
+++ server_src/game_src/explosion_callback.cpp
@@ -0,0 +1,10 @@
+#include "explosion_callback.h"
+
+float b2RayCastExplosionCallback::ReportFixture(b2Fixture* fixture , const b2Vec2& point , const b2Vec2& normal , float fraction){
+    body = fixture->GetBody();
+    return fraction;
+}
+
+
+b2RayCastExplosionCallback::~b2RayCastExplosionCallback(){}
+
--- server_src/game_src/explosion_callback.h
+++ server_src/game_src/explosion_callback.h
@@ -7,11 +7,12 @@ class b2RayCastExplosionCallback : public b2RayCastCallback {
     friend class Explosion;
     b2Body* body;
 
-    float ReportFixture(b2Fixture* fixture , const b2Vec2& point , const b2Vec2& normal , float fraction){
-        body = fixture->GetBody();
-        return fraction;
-    }
+    float ReportFixture(b2Fixture* fixture , const b2Vec2& point , const b2Vec2& normal , float fraction);
+
+    ~b2RayCastExplosionCallback();
 };
+
+
 
 
 #endif // EXPLOSION_CALLBACK_H
